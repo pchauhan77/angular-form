8ae32296483eb594c63a043786b80d89ca69cd42
Pradeep Chauhan <pradeep.chauhan67@gmail.com>
2022-02-11 01:46:23 +0530

    first commit

--- src/app/add-story/add-story.component.ts
+++ src/app/add-story/add-story.component.ts
@@ -1,15 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
+import { NgModule } from '@angular/core';
 @Component({
   selector: 'app-add-story',
   templateUrl: './add-story.component.html',
   styleUrls: ['./add-story.component.css']
 })
 export class AddStoryComponent implements OnInit {
-
-  constructor() { }
-
+storyForm:any={};
+constructor( public http:HttpClient) { }
+ 
   ngOnInit(): void {
   }
-
+  onSubmit(){
+    console.log(this.storyForm);
+this.http.post('http://localhost:5000/form', this.storyForm).subscribe(data=>{
+  console.log(data);
+  
+})
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,8 @@ import { AddStoryComponent } from './add-story/add-story.component';
 import { AddStorySubgroupComponent } from './add-story-subgroup/add-story-subgroup.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AddButtonsComponent } from './add-buttons/add-buttons.component';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +17,7 @@ import { AddButtonsComponent } from './add-buttons/add-buttons.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule
+    NgbModule,FormsModule,HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
